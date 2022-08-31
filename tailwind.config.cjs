8fbdcd2d54bd49b8b295aff3f26846b8d9f8881c
Theo Goedert <tpgoedert@outlook.com>
2022-08-31 13:41:22 +0200

    add custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        sans: ['Fragment sans', 'sans-serif'],
+        serif: ['Fragment glare', 'serif'],
+      },
+      fontSize: {
+        dynamicxl: ['clamp(2.5rem, 10vw, 4.5rem)', '1'],
+        dynamicl: ['clamp(1.25rem, 10vw, 3rem)', '1'],
+        dynamic: ['clamp(1rem, 10vw, 2.25rem)', '1.2'],
+      },
+    },
 	},
 	plugins: [],
 }
